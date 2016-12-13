d24a977f598d1adddd23d1d37e54081af0ce94f8
OwlAford <244734834@qq.com>
2016-12-13 09:03:06 +0800

    update styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,11 @@ import './Header.scss'
 export const Header = () => (
   <div>
     <h1>Blade Starter Kit</h1>
-    <IndexLink to='/' activeClassName='route--active'>
+    <IndexLink to='/' activeClassName='route-active'>
       Home
     </IndexLink>
     {' · '}
-    <Link to='/counter' activeClassName='route--active'>
+    <Link to='/counter' activeClassName='route-active'>
       Counter
     </Link>
   </div>
