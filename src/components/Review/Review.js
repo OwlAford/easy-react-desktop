1a34a7df38260f7a8f108c6795c61906ccc88d8a
OwlAford <1005476098@qq.com>
2016-12-18 18:00:20 +0800

    add some details

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,4 +1,53 @@
 ﻿import React, { Component } from 'react'
+import { Collapse, Table } from 'antd'
+const Panel = Collapse.Panel
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`
+
+function callback(key) {
+  console.log(key)
+}
+
+const columns = [{
+  title: '邮箱',
+  dataIndex: 'email',
+  key: 'email'
+}, {
+  title: '密码',
+  dataIndex: 'password',
+  key: 'password'
+}, {
+  title: '密码确认',
+  dataIndex: 'confirm',
+  key: 'confirm'
+}, {
+  title: '手机号码',
+  dataIndex: 'phone',
+  key: 'phone'
+}, {
+  title: '昵称',
+  dataIndex: 'nickname',
+  key: 'nickname'
+}, {
+  title: '验证码',
+  dataIndex: 'captcha',
+  key: 'captcha'
+}, {
+  title: '地址',
+  key: 'residence',
+  dataIndex: 'residence',
+  render: (text) => (
+    <span>
+      <span>{text[0]}</span>/
+      <span>{text[1]}</span>/
+      <span>{text[2]}</span>
+    </span>
+  )
+}]
 
 export default class Review extends Component {
 
@@ -11,31 +60,33 @@ export default class Review extends Component {
 
   render() {
     let { regData } = this.props
+    regData.key = 1
     let dataList = (regData) => (
       <div>
-        {regData.email}
-        {regData.confirm}
-        {regData.password}
-        {regData.residence[0]}
-        {regData.residence[1]}
-        {regData.residence[2]}
-        {regData.nickname}
-        {regData.phone}
-        {regData.captcha}
+        <Table columns={columns} dataSource={[regData]} />
       </div>
     ) 
 
     let noData = () => (
-      <div>
-        No Data！
+      <div style={{width: '100%', textAlign: 'center', fontSize: '20px', padding: '20px 0'}}>
+        暂未填写信息！
       </div>
     )
 
     return (
       <div>
+        <Collapse defaultActiveKey={['1']} onChange={callback} style={{margin: '20px 0'}}>
+          <Panel header="This is panel header 1" key="1">
+            <p>{text}</p>
+          </Panel>
+          <Panel header="This is panel header 2" key="2">
+            <p>{text}</p>
+          </Panel>
+          <Panel header="This is panel header 3" key="3">
+            <p>{text}</p>
+          </Panel>
+        </Collapse>
         {regData ? dataList(regData) : noData()}
-        <h1>输入电话号码：</h1>
-        <input type='tel' value="" placeholder="请输入号码"/>
       </div>
     )
   }
